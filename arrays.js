ab266fd583e95907d3339a34079cb890db2ae055
Charity Jackson <charitylennjackson@yahoo.com>
2018-04-30 19:11:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,6 +30,8 @@ var chocolateBars =  ["snickers", "hundred grand", "kitkat", "skittles"];
      return array[n];
     }
     
-    
+    function destructivelyRemoveElementFromBeginningOfArray{
+      array.shfit
+    }
     
    
